972b99cfa24a804864eb958014cef37a171e9d9e
Sahir Ul Hassan <sahirulhasan2002@gmail.com>
2025-10-27 00:59:22 +0500

    lots of changes

--- src/client.js
+++ src/client.js
@@ -1,14 +1,20 @@
-import pkg from 'whatsapp-web.js';
-const { Client, LocalAuth, MessageMedia } = pkg
+import pkg from "whatsapp-web.js";
+const { Client, LocalAuth, MessageMedia } = pkg;
 
 const client = new Client({ authStrategy: new LocalAuth() });
-client.state = "Unknown"
-client.on('authenticated', () => client.state = "Just logged in")
-client.on('ready', () => client.state = "Ready")
-client.on('disconnected', () => client.state = "Disconnected")
-client.on('auth_failure', () => client.state = "Auth Failure")
-client.on('qr', (qr) => {
-    client.state = "Logged out - Scan QR"
-    client.qr = qr
-})
+client.state = "Unknown";
+client.on(
+  "authenticated",
+  () => (client.state = "Just logged in. Please wait for ready status."),
+);
+client.on(
+  "ready",
+  () => (client.state = "Ready - You can start sending messages now."),
+);
+client.on("disconnected", () => (client.state = "Disconnected"));
+client.on("auth_failure", () => (client.state = "Auth Failure"));
+client.on("qr", (qr) => {
+  client.state = "Logged out - Scan QR to login";
+  client.qr = qr;
+});
 export default client;
--- src/whatsapp-controllers.js
+++ src/whatsapp-controllers.js
@@ -3,17 +3,19 @@ import qrcode from "qrcode";
 import AppError from "./utils/AppError.js";
 
 export async function handleGetStatus(req, res) {
-  const altState = await client.getState();
-  res.status(200).json({ state: client.state, altState });
+  res.status(200).json({ state: client.state });
 }
 
 export async function handleGetQRCode(req, res) {
   if (!client.qr) {
-    throw new AppError(404, "QR code not available.");
+    throw new AppError(
+      404,
+      "QR code not yet available. Please try again later.",
+    );
   }
   let qrBuffer = null;
   try {
-    qrBuffer = await qrcode.toBuffer(client.qr, { type: "png" }); // Store the buffer
+    qrBuffer = await qrcode.toBuffer(client.qr, { type: "png" });
   } catch (err) {
     throw new AppError(500, "Failed to generate QR code PNG Buffer.", err);
   }
@@ -24,5 +26,8 @@ export async function handleGetQRCode(req, res) {
 
 export async function handleLogout(req, res) {
   await client.logout();
+  client.state = "Logged out. Restarting";
+  client.qr = null;
   res.status(204).json({ message: "Logged out successfully" });
+  client.initialize();
 }
