f161ccb8cf234015afe7836f53f0c30afa62a6fd
Sahir Ul Hassan <sahirulhasan2002@gmail.com>
2025-10-29 21:19:12 +0500

    refactored the message sending logic

--- src/whatsapp-services.js
+++ src/whatsapp-services.js
@@ -6,64 +6,116 @@ import fs from "fs";
 import pLimit from "p-limit";
 const limit = pLimit(5);
 
-export async function sendToNumber(number, payload) {
-  const id = await client.getNumberId(number);
-  if (!id) return false;
-  else if (payload.message && !payload.files) {
-    await client.sendMessage(id._serialized, payload.message);
-    return true;
-  } else if (payload.files.length > 1 && !payload.message) {
-    const results = await Promise.allSettled(
-      payload.files.map((file) =>
-        limit(() => client.sendMessage(id._serialized, file)),
-      ),
-    );
-    return true;
-  } else if (!payload.message && payload.files.length === 1) {
-    await client.sendMessage(id._serialized, payload.files[0]);
-    return true;
-  } else if (payload.message && payload.files.length === 1) {
-    await client.sendMessage(id._serialized, payload.files[0], {
-      caption: payload.message,
-    });
-    return true;
-  } else if (payload.message && payload.files.length > 1) {
-    const results = await Promise.allSettled(
-      payload.files.map((file) =>
-        limit(() => client.sendMessage(id._serialized, file)),
-      ),
-    );
-    await client.sendMessage(id._serialized, payload.message);
-    return true;
-  }
-}
-
 async function sleep() {
   const jitter = 6000 + Math.floor(Math.random() * 4000);
   return new Promise((resolve) => setTimeout(resolve, jitter));
 }
 
+async function sendWithOnlyMedia(numbers, media) {
+  return Promise.allSettled(
+    numbers.map((number) =>
+      limit(async () => {
+        for (const file of media) {
+          await client.sendMessage(number, file);
+          await sleep();
+        }
+        return number;
+      }),
+    ),
+  );
+}
+
+async function sendWithOnlyMessage(numbers, message) {
+  return Promise.allSettled(
+    numbers.map((number) =>
+      limit(async () => {
+        await client.sendMessage(number, message);
+        await sleep();
+        return number;
+      }),
+    ),
+  );
+}
+
+async function sendWithMessageAndOneMedia(numbers, message, media) {
+  return Promise.allSettled(
+    numbers.map((number) =>
+      limit(async () => {
+        await client.sendMessage(number, media, { caption: message });
+        await sleep();
+        return number;
+      }),
+    ),
+  );
+}
+
+async function sendWithMessageAndMultipleMedia(numbers, message, media) {
+  return Promise.allSettled(
+    numbers.map((number) =>
+      limit(async () => {
+        for (const file of media) {
+          await client.sendMessage(number, file);
+          await sleep();
+        }
+        await client.sendMessage(number, message);
+        return number;
+      }),
+    ),
+  );
+}
+
 export async function sendMessages(numbers, payload) {
   if (!numbers) throw new AppError(400, "Please provide at least 1 number.");
-  numbers = numbers.split(",").map((n) => n.trim());
+  const regex = /^(?:[1-9]\d{6,14})(?:,\s*(?:[1-9]\d{6,14}))*$/;
+  if (!regex.test(numbers)) throw new AppError(400, "Invalid number format.");
   if (client.state !== "Ready - You can start sending messages now.")
     throw new AppError(401, "Client not ready.");
   if (!(payload.message || payload.files))
     throw new AppError(400, "Please provide at least 1 message or 1 file.");
-  if (payload.files)
-    payload.files = await Promise.all(
+
+  numbers = numbers.split(",").map((n) => n.trim());
+
+  const numberIds = await Promise.all(
+    numbers.map(async (num) => {
+      const id = await client.getNumberId(num);
+      return id?._serialized;
+    }),
+  );
+  const validNumbers = numberIds.filter(Boolean);
+  if (!validNumbers.length)
+    throw new AppError(400, "No valid WhatsApp numbers found.");
+
+  // prepare files
+  if (payload.files && payload.files.length > 0) {
+    const fileResults = await Promise.allSettled(
       payload.files.map((f) => MessageMedia.fromFilePath(f.path)),
     );
-  //let log = {};
-  const results = await Promise.allSettled(
-    numbers.map((num) => limit(() => sendToNumber(num, payload))),
-  );
-  // for (const number of numbers) {
-  //   const sent = await sendToNumber(number, payload);
-  //   log[number] = sent ? "Sent" : "Failed";
-  //   await sleep();
-  // }
-  fs.mkdirSync("logs/", { recursive: true });
+    payload.files = fileResults
+      .filter((r) => r.status === "fulfilled")
+      .map((r) => r.value);
+  } else {
+    payload.files = [];
+  }
+
+  let results;
+  if (payload.files.length === 0 && payload.message)
+    results = await sendWithOnlyMessage(validNumbers, payload.message);
+  else if (payload.files.length > 0 && !payload.message)
+    results = await sendWithOnlyMedia(validNumbers, payload.files);
+  else if (payload.files.length === 1 && payload.message)
+    results = await sendWithMessageAndOneMedia(
+      validNumbers,
+      payload.message,
+      payload.files[0],
+    );
+  else
+    results = await sendWithMessageAndMultipleMedia(
+      validNumbers,
+      payload.message,
+      payload.files,
+    );
+
+  fs.mkdirSync("logs", { recursive: true });
   fs.writeFileSync(
     `logs/${payload.timestamp}.json`,
     JSON.stringify(results, null, 2),
